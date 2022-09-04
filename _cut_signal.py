69427a37b6922bb76719f7ff8aaa488add651443
AlexProher <alexproher@gmail.comm>
2022-09-04 11:06:09 +0300

    cleared code in 	modified:   Data_comparison.ipynb
    defined finctions :
        find_slope()
        cut_signal()
        my_interpolation()
        in
    	modified:   _cut_signal.py

--- _cut_signal.py
+++ _cut_signal.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy import interpolate
 
 ##
@@ -9,29 +10,31 @@ from scipy import interpolate
 
 
 def find_slope(sign1, sign2): #sign1 - Сигнал деформаций (опорный), sign2 - Сигнал температуры
-    tr1 = sign1.max()*0.01
-    tr2 = sign2.max()*0.01
-    bin_sig1 = [1 if x>tr1 else 0 for x in sign1]
-    bin_sig2 = [1 if x>tr2 else 0 for x in sign2]
-    return (bin_sig1.index(1), bin_sig2.index(1))
+    #Функция ищет восходящий фронт сигнала по порогу чувствительности 0.01 от максимальной величины сигнала
+    tr1 = sign1.max()*0.1
+    tr2 = sign2.max()*0.055
+
+    return (sign1[sign1 > tr1].index[0], 
+            sign2[sign2 > tr2].index[0])
 
 def my_interpolation(t, data):
-    xnew = np.arange(0,len(data)-1,t*1e-9)
-    xnew[-1] = round(xnew[-1])
-    x = np.arange(len(data))
-    y = np.array(data)
-    f = interpolate.interp1d(x, y, kind = 'quadratic')
+    xnew = np.arange(0,max(t),500*1e-9) #500 нс - величина обратная Частоте дискретизации 2ГГц
+    f = interpolate.interp1d(np.array(t), np.array(data), kind = 'quadratic')
     ynew = f(xnew)
-    return (xnew, ynew)
+    return (pd.DataFrame({'time': xnew,'value': ynew}))
 
 def cut_signal(signal1, signal2, time1, time2):
+    #Функция отрезвет сигнал температуры в соответствии с длиной сигнала деформаций
     #сначала интерполируем второй синал чтобы его можно было двигать плавно
-    interpolated_signal2 = my_interpolation(signal2, 500)   #500 нс - величина обратная Частоте дискретизации
-    p1, p2 = find_slope(signal1, interpolated_signal2)
-
+    new_signal2 = my_interpolation(time2, signal2)   
+    p1, p2 = find_slope(signal1, new_signal2.value)
+    #Сдвигаем сигнал температуры влево так чтобы время обнаруженного подъема р1 совпало со временем р1
+    new_signal2.time = new_signal2.time - new_signal2.time[p2] + time1[p1]
+    #new_signal2.time = new_signal2.time - 107e-6
+    #отрезвем сигнал температуры сначала так чтобы были только положительные времена
+    new_signal2 = new_signal2[new_signal2.time >= 0]
+    #отрезаем лишнюю часть после полезного сигнала
+    new_signal2 = new_signal2[:len(signal1)]
     
-
- 
-    
-    return ()
+    return (new_signal2)
     
